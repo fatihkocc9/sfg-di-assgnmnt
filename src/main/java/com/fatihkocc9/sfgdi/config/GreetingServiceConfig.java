bee8bb65444e0f5736b7242d32a48221784e6c0c
Fatih Koç <fatihkoc@hotmail.it>
2021-08-30 12:58:37 +0300

    Java configuration

---
+++ src/main/java/com/fatihkocc9/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,27 @@
+package com.fatihkocc9.sfgdi.config;
+
+
+import com.fatihkocc9.sfgdi.services.ConstructorGreetingService;
+import com.fatihkocc9.sfgdi.services.PropertyInjectedGreetingService;
+import com.fatihkocc9.sfgdi.services.SetterInjectedGreetingService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GreetingServiceConfig {
+
+    @Bean
+    ConstructorGreetingService constructorGreetingService(){
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService(){
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService(){
+        return new SetterInjectedGreetingService();
+    }
+}
